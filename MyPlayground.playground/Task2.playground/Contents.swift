859a20d9ab173fcd3003904cde8aa267e8dc13d4
IlgazUygur01 <180198104+IlgazUygur01@users.noreply.github.com>
2024-09-05 00:51:54 +0300

    Add files via upload

---
+++ MyPlayground.playground/Task2.playground/Contents.swift
@@ -0,0 +1,10 @@
+let grade1 = 70
+let grade2 = 40
+
+let average = (grade1 + grade2) / 2
+
+if average >= 50 {
+    print("Passed")
+} else {
+    print("Failed")
+}
---
+++ MyPlayground.playground/Task3.playground/Contents.swift
@@ -0,0 +1,6 @@
+
+let name = "Ilgaz Uygur"
+
+for _ in 1...10 {
+    print(name)
+}
