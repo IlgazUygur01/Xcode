2d2f02a066e299ad129fc8099c4c600f71e998e8
IlgazUygur01 <180198104+IlgazUygur01@users.noreply.github.com>
2024-09-05 00:45:01 +0300

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+let weather = "rain"
+
+if weather == "rain" {
+    print("Take an umbrella.")
+} else if weather == "snow" {
+    print("Take a scarf.")
+} else if weather == "sunny" {
+    print("Take sunglasses.")
+} else {
+    print("You don't have to do anything.")
+}
+
